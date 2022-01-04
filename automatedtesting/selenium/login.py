6b952ef0a5f0df2b63b171318be4d73757067929
Srdjan <srdan.ljepic@tttech-auto.com>
2022-01-04 17:03:31 +0100

    add selenium tests in ci-cd

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,4 +1,5 @@
 # #!/usr/bin/env python
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
@@ -14,45 +15,45 @@ shopping_cart = {
 
 
 def initialize_browser():
-    print ('Starting the browser...')
+    logging.info('Starting the browser...')
     #--uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    logging.info('Browser started successfully. Navigating to the demo page to login.')
     return driver
 
 # Start the browser and login with standard_user
 def login (user, password, driver):
     url = 'https://www.saucedemo.com/'
     driver.get(url)
-    print("Succesfully navigated to " + url)
+    logging.info("Succesfully navigated to " + url)
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
     driver.find_element_by_css_selector("input[id='login-button']").click()
 
     login_page_element = driver.find_element_by_css_selector("div[id='header_container'] > div[class='header_secondary_container'] > span.title").text
     assert "PRODUCTS" == login_page_element
-    print("Succesfully loggeed to " + url + " with user " + user)
+    logging.info("Succesfully loggeed to " + url + " with user " + user)
 
 
 def add_items_to_shopping_cart(driver):
     for item, item_id in shopping_cart.items():
         css_selector = "button[id='add-to-cart-" + item_id + "']"
         driver.find_element_by_css_selector(css_selector).click()
-        print(item + " has been added to a shopping cart.")
+        logging.info(item + " has been added to a shopping cart.")
 
 def remove_items_from_shopping_cart(driver):
     for item, item_id in shopping_cart.items():
         css_selector = "button[id='remove-" + item_id + "']"
         driver.find_element_by_css_selector(css_selector).click()
-        print(item + " has been removed to a shopping cart.")
+        logging.info(item + " has been removed to a shopping cart.")
 
 def test_if_items_are_in_shopping_cart(driver):
     shopping_cart_items = driver.find_element_by_css_selector("div[id='shopping_cart_container'] > a > span.shopping_cart_badge").text
     assert '6' in shopping_cart_items
-    print("All products are successfully added to a cart. Totally found " + shopping_cart_items + " items.")
+    logging.info("All products are successfully added to a cart. Totally found " + shopping_cart_items + " items.")
 
 def test_if_shoping_cart_is_empty(driver):
     try:
@@ -62,26 +63,27 @@ def test_if_shoping_cart_is_empty(driver):
         cart_badge_not_found = True
 
     assert cart_badge_not_found
-    print("Shopping cart is empty.")
+    logging.info("Shopping cart is empty.")
 
 def main():
-    print("-------------------------------")
-    print("Starting execution")
-    print("-------------------------------")
+    logging.basicConfig(filename="selenium-test.log", level=logging.INFO)
+    logging.info("-------------------------------")
+    logging.info("Starting execution")
+    logging.info("-------------------------------")
     driver = initialize_browser()
     ###login to page
     login('standard_user', 'secret_sauce', driver)
-    print("Check if shopping cart is empty...")
+    logging.info("Check if shopping cart is empty...")
     test_if_shoping_cart_is_empty(driver)
-    print("Adding items to shopping cart...")
+    logging.info("Adding items to shopping cart...")
     add_items_to_shopping_cart(driver)
-    print("Check if items are succesfully added to a shopping cart")
+    logging.info("Check if items are succesfully added to a shopping cart")
     test_if_items_are_in_shopping_cart(driver)
-    print("Removing objects from shopping cart...")
+    logging.info("Removing objects from shopping cart...")
     remove_items_from_shopping_cart(driver)
-    print("Check if shopping cart is empty...")
+    logging.info("Check if shopping cart is empty...")
     test_if_shoping_cart_is_empty(driver)
-    print("End of execution.")
+    logging.info("End of execution.")
 
 
 if __name__ == '__main__':
